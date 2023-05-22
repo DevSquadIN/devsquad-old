62b25698f35d89428b63c8911858bc5b5b109dfc
Joy Poddar <joypoddar.sd@gmail.com>
2023-05-22 15:52:31 +0530

    feat: devsquad logo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -10,13 +10,21 @@ import Login from "./login";
 const Navbar = ({ home, landing }) => {
   const { signOut } = useSignOut();
   return (
-    <nav className="mx-auto flex max-w-5xl items-center justify-between py-6">
+    <nav className="mx-auto flex max-w-7xl items-center justify-between py-6">
       {/* logo */}
       <Link
         href="/lessons"
-        className="font-serif font-bold tracking-wider text-gray-900"
+        className="font-sans text-3xl font-medium text-indigo-800"
       >
-        nxt100
+        <div className="flex flex-col">
+          <p className="flex">
+            <span>devsquad</span>
+            <span className="ml-0.5 text-xs font-extralight">beta</span>
+          </p>
+          <span className="text-xs font-extralight tracking-[0.23em]">
+            collab.build.learn
+          </span>
+        </div>
       </Link>
       {/* logout button */}
       <div className="flex items-center gap-3">
