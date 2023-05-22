7afdcb3ab912360b84a5fd60af651f2056d43768
Joy Poddar <joypoddar.sd@gmail.com>
2023-05-22 15:55:42 +0530

    feat: minimal navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,21 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
-import { HiArrowLeftOnRectangle } from "react-icons/hi2";
-import discord from "../public/images/discord.svg";
-import twitter from "../public/images/twitter.svg";
-import Menu from "./menu";
-import { useSignOut } from "@nhost/nextjs";
-import Login from "./login";
 
-const Navbar = ({ home, landing }) => {
-  const { signOut } = useSignOut();
+const Navbar = () => {
   return (
     <nav className="mx-auto flex max-w-7xl items-center justify-between py-6">
       {/* logo */}
-      <Link
-        href="/lessons"
-        className="font-sans text-3xl font-medium text-indigo-800"
-      >
+      <Link href="/" className="font-sans text-3xl font-medium text-indigo-800">
         <div className="flex flex-col">
           <p className="flex">
             <span>devsquad</span>
@@ -26,25 +15,6 @@ const Navbar = ({ home, landing }) => {
           </span>
         </div>
       </Link>
-      {/* logout button */}
-      <div className="flex items-center gap-3">
-        <Link href="#">
-          <Image className="h-5 w-7" src={discord} alt="discord" />
-        </Link>
-        <Link href="#">
-          <Image className="h-5 w-7" src={twitter} alt="twitter" />
-        </Link>
-        {home ? (
-          <button onClick={signOut}>
-            <HiArrowLeftOnRectangle className="h-6 w-6" />
-            <span className="sr-only">Sign Out</span>
-          </button>
-        ) : landing ? (
-          <Login />
-        ) : (
-          <Menu />
-        )}
-      </div>
     </nav>
   );
 };
